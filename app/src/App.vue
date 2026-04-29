6e69dc6e047962b1bf2823b315872400ae775ca1
Daniil Zaburunov <zaburunov.2000@mail.ru>
2026-04-30 00:28:48 +0400

    add wrapp for app

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
-import Hello from './components/Hello.vue';
+import ButtonTheme from './components/ButtonTheme.vue';
+
 
 
 </script>
 
 <template>
-<Hello />
+  <div class="wrapp">
+    <ButtonTheme />
+  </div>
 </template>
+<style scoped>
+@reference "@/style.css";
+
+.wrapp {
+  @apply p-5;
+}
+</style>
