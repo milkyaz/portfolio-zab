d5b8396dbc9d4773e133a31ed8233284287febfa
Daniil Zaburunov <zaburunov.2000@mail.ru>
2026-04-30 00:27:17 +0400

    configure path aliases

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
+import path from 'node:path'
 
 export default defineConfig({
   plugins: [tailwindcss(), vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
