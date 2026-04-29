5b08281ae3b9c1b9e37947f68169cd4443521ac9
Daniil Zaburunov <zaburunov.2000@mail.ru>
2026-04-30 00:31:15 +0400

    update style for button-theme

---
+++ app/src/components/ButtonTheme.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Icon } from "@iconify/vue";
+
+</script>
+
+<template>
+    <div class="flex flex-row justify-center">
+        <button class="btn btn-lg btn-circle btn-outline btn-primary">
+            <Icon icon="fluent:dark-theme-20-regular" />
+        </button>
+    </div>
+</template>
