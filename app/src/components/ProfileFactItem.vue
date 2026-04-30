200f7da67d923c577ae9d3230615d48f0fedc050
Daniil Zaburunov <daniil.zaburunov@fin-frame.ru>
2026-04-30 13:16:59 +0400

    add profile-fact-list

---
+++ app/src/components/ProfileFactItem.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+
+type Props = {
+    title: string
+    description: string
+}
+const {title, description} = defineProps<Props>()
+</script>
+
+<template>
+    <div class="text-center font-[--font-poppins]">
+        <h2 class="text-base text-[#C3C7CB]">{{ title }}</h2>
+        <p class="text-sm text-[#A3ABB2]">{{ description }}</p>
+    </div>
+</template>
+
+<style scoped>
+@reference "@/style.css";
+
+</style>
