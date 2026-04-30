dcdf87cd898c3a766ca5f1d142491fee3044af40
Daniil Zaburunov <daniil.zaburunov@fin-frame.ru>
2026-04-30 12:48:33 +0400

    mob-1: add profile-info

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import ButtonTheme from './components/ButtonTheme.vue';
+import ProfileInfo from './components/ProfileInfo.vue';
 
 
 
@@ -7,13 +8,18 @@ import ButtonTheme from './components/ButtonTheme.vue';
 
 <template>
   <div class="wrapp">
-    <ButtonTheme />
+    <header>
+      <ButtonTheme />
+    </header>
+    <main>
+      <ProfileInfo />
+    </main>
   </div>
 </template>
 <style scoped>
 @reference "@/style.css";
 
 .wrapp {
-  @apply p-5;
+  @apply flex flex-col gap-7.5 p-5;
 }
 </style>
