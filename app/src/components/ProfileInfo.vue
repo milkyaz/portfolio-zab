0c228dc73a7f8ad9f1a37bc8bd7326e43af228c7
Daniil Zaburunov <daniil.zaburunov@fin-frame.ru>
2026-06-18 13:05:25 +0400

    mob-3: refactor component and update style

--- app/src/components/ProfileInfo.vue
+++ app/src/components/ProfileInfo.vue
@@ -1,28 +1,42 @@
 <script setup lang="ts">
 import { Icon } from '@iconify/vue';
 
+const social = [
+  {
+    link: 'https://github.com',
+    icon: 'mdi:github',
+    key: 'github'
+  },
+  {
+    link: 'https://telegram.me',
+    icon: 'mdi:telegram',
+    key: 'telegram'
+  },
+  {
+    link: 'mailto:mail@example.com',
+    icon: 'mdi:gmail',
+    key: 'mail'
+  },
+  {
+    link: 'https://vk.com',
+    icon: 'mdi:vk',
+    key: 'vk'
+  },
+]
 </script>
 
 <template>
     <div class="flex flex-row justify-center">
         <div class="flex flex-col items-center gap-10">
-            <div class="bg-[url(./assets/profile-photo.svg)] w-31 h-31"></div>
             <div>
                 <h1 class="profile-info__title">Zaburunov Daniil</h1>
                 <p class="text-center text-base-content/60 text-sm mt-3 mb-5 leading-relaxed tracking-wide">Frontend Developer</p>
-                <div class="flex flex-row justify-center gap-6">
-                    <a href="https://github.com" target="_blank" rel="noopener noreferrer" class="text-base-content hover:text-primary transition-all duration-300 ease-out hover:scale-110" aria-label="GitHub">
-                        <Icon icon="mdi:github" width="24" />
+                <div  class="flex flex-row justify-center gap-10">
+                  <template v-for="platform in social" :key="platform.key">
+                    <a :href="platform.link" target="_blank" rel="noopener noreferrer" class="text-base-content hover:text-primary transition-all duration-300 ease-out hover:scale-110" aria-label="GitHub">
+                      <Icon :icon="platform.icon" width="35" />
                     </a>
-                    <a href="https://telegram.me" target="_blank" rel="noopener noreferrer" class="text-base-content hover:text-primary transition-all duration-300 ease-out hover:scale-110" aria-label="Telegram">
-                        <Icon icon="mdi:telegram" width="24" />
-                    </a>
-                    <a href="mailto:mail@example.com" class="text-base-content hover:text-primary transition-all duration-300 ease-out hover:scale-110" aria-label="Email">
-                        <Icon icon="mdi:gmail" width="24" />
-                    </a>
-                    <a href="https://vk.com" target="_blank" rel="noopener noreferrer" class="text-base-content hover:text-primary transition-all duration-300 ease-out hover:scale-110" aria-label="VK">
-                        <Icon icon="mdi:vk" width="24" />
-                    </a>
+                  </template>
                 </div>
             </div>
         </div>
